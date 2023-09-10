f355689a6c4b9562ab27bf832397ce2770a226a4
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2023-09-10 18:53:21 -0300

    Arquivos aula 04 MySQL

--- 20230910-aula04/script02.sql
+++ 20230910-aula04/script02.sql
@@ -30,3 +30,20 @@ FROM customers
 LEFT JOIN orders
 USING (customerNumber)
 WHERE orders.orderNumber IS NULL;
+
+-- Desafio
+-- Trazer os clientes que nunca efetuaram um pagamento
+-- Dica: Utilize as tabelas customers e payments
+
+SELECT
+	customers.customerNumber,
+    customers.customerName,
+    payments.amount
+FROM customers
+LEFT JOIN payments
+USING (customerNumber)
+WHERE payments.customerNumber IS NULL;
+
+-- Desafio 2
+-- Trazer, para cada cliente, a quantidade de pedidos que ele fez, e qual é o valor médio
+-- dos pedidos
